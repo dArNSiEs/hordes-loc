8a47f82f3736ccf4dd25d2aa39d05626a75dae92
dArNSiEs <49496964+dArNSiEs@users.noreply.github.com>
2019-04-13 08:28:27 -0400

    Update quiver.js

--- loc/items/quiver.js
+++ loc/items/quiver.js
@@ -4,7 +4,8 @@ export default [
     description: {
       en: 'Soft, full of holes. Arrows keep falling out.',
       ru: 'Мягкий, дырявый. Стрелки продолжают выпадать.',
-      es: 'Si, los calcetines tambien pueden ser una aljaba.'
+      es: 'Si, los calcetines tambien pueden ser una aljaba.',
+      fr: 'Doux, plein de trous. Les flèches continuent à tomber.'
     }
   },
 
@@ -13,7 +14,8 @@ export default [
     description: {
       en: 'Linen sack covered in stiff leather.',
       ru: 'Льняной мешочек обтянут жесткой кожей.',
-      es: 'Una bolsa de lana con cuero.'
+      es: 'Una bolsa de lana con cuero.',
+      fr: 'Sac de lin recouvert de cuir rigide.'
     }
   },
 
@@ -22,7 +24,8 @@ export default [
     description: {
       en: 'Used for when all else fails.',
       ru: 'Используется, когда ничего не помогает.',
-      es: 'Nunca se queda sin flechas.'
+      es: 'Nunca se queda sin flechas.',
+      fr: 'Utilisé quand tout le reste échoue.'
     }
   },
 
@@ -31,7 +34,8 @@ export default [
     description: {
       en: 'Elven quiver coated in shimmering diamond.',
       ru: 'Эльфийский колчан, покрытый мерцающим бриллиантом.',
-      es: 'Un carcaj creado por los duendes cubrierto en diamante.'
+      es: 'Un carcaj creado por los duendes cubrierto en diamante.',
+      fr: 'Carquois elfique recouvert de diamant scintillant.'
     }
   },
 
@@ -40,7 +44,8 @@ export default [
     description: {
       en: 'Every time you pull an arrow out, it hisses at you.',
       ru: 'Каждый раз, когда вы вытаскиваете стрелу, она шипит на вас.',
-      es: 'Serpientes ¿Por qué tenía que ser serpientes?'
+      es: 'Serpientes ¿Por qué tenía que ser serpientes?',
+      fr: 'Chaque fois que vous tirez une flèche, ça siffle contre toi.'
     }
   }
 
