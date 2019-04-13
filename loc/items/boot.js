8c452b1d2a8bcecfd2bcd79a202f951945d24cab
dArNSiEs <49496964+dArNSiEs@users.noreply.github.com>
2019-04-13 13:53:59 -0400

    Update boot.js

--- loc/items/boot.js
+++ loc/items/boot.js
@@ -4,7 +4,8 @@ export default [
     description: {
       en: 'At least you are wearing something on those feet. ',
       ru: 'По крайней мере, вы носите что-то на этих ногах.',
-      es: 'A lo menos puede tirarlo para hacer daño significativo.'
+      es: 'A lo menos puede tirarlo para hacer daño significativo.',
+      fr: 'Au moins tu portes quelque chose sur ces pieds.'
     }
   },
 
@@ -13,7 +14,9 @@ export default [
     description: {
       en: 'Soles made from cloth, will probably tear after a few hours.',
       ru: 'Подошвы сделанные из ткани, вероятно, порвутся через несколько часов.',
-      es: 'OI. Estos son calcetines. ¡Dame mis chanclas!'
+      es: 'OI. Estos son calcetines. ¡Dame mis chanclas!',
+      fr: 'Semelles en tissu, va probablement se déchirer après quelques heures.'
+      
     }
   },
 
@@ -22,7 +25,8 @@ export default [
     description: {
       en: 'The soft leather keeps your feet warm while providing a little protection from low blows.',
       ru: 'Мягкая кожа сохраняет ваши ноги в тепле, обеспечивая небольшую защиту от низких ударов.',
-      es: 'Con el cuero de un toro fuerte, estás protegido por algunas más días.'
+      es: 'Con el cuero de un toro fuerte, estás protegido por algunas más días.',
+      fr: 'Le cuir souple garde vos pieds au chaud, tout en offrant un peu de protection contre les coups bas.'
     }
   },
 
@@ -31,7 +35,8 @@ export default [
     description: {
       en: 'Clearly lacking in imagination, the maker made boots from the bones of a very large creature...',
       ru: 'Очевидно, что не хватает воображения, создатель сделал сапоги из костей очень крупного существа ...',
-      es: 'Claramente sin imaginación, el zapatero uso los huesos de una criatura grande.'
+      es: 'Claramente sin imaginación, el zapatero uso los huesos de una criatura grande.',
+      fr: 'Manque d\'imagination, le créateur a fabriqué des bottes à partir des os d\'une très grande créature...'
     }
   },
 
@@ -40,7 +45,8 @@ export default [
     description: {
       en: 'Scales line line the outside of your footwear, catching glares in the sun',
       ru: 'Чешуя выравнивают внешнюю сторону вашей обуви, ловя блики на солнце',
-      es: 'Las escamas de una lagartija te da botas impermeables.'
+      es: 'Las escamas de una lagartija te da botas impermeables.',
+      fr: 'Les balances bordent l\'extérieur de vos chaussures, attraper des fusées éclairantes au soleil'
     }
   },
 
@@ -49,7 +55,8 @@ export default [
     description: {
       en: 'A pleasant pattern made to paralyze your foes....wonder if it works',
       ru: 'Приятный узор, созданный, чтобы парализовать ваших врагов .... интересно, работает ли он',
-      es: '¡Eyyy, ahora puedes correr sin desmayar con zapatos reales!'
+      es: '¡Eyyy, ahora puedes correr sin desmayar con zapatos reales!',
+      fr: 'Un dessin plaisant fait pour paralyser vos ennemis....me demande si ça marche'
     }
   },
 
@@ -58,7 +65,8 @@ export default [
     description: {
       en: 'Taken from war veterans who clearly didn\'t find enough protection from these greaves.',
       ru: 'Взято у ветеранов войны, которые явно не нашли достаточной защиты от этих наголенников.',
-      es: 'No protegieron sus dueños anteriores tan bien, tal vez funcionan mejor ahora.'
+      es: 'No protegieron sus dueños anteriores tan bien, tal vez funcionan mejor ahora.',
+      fr: 'Pris à partir des vétérans de guerre qui clairement n\'a pas trouvé assez de protection de ces bottes.'
     }
   },
 
@@ -67,7 +75,8 @@ export default [
     description: {
       en: 'Chunky in its looks, probably very uncomfortable',
       ru: 'Коренастый во внешности, наверное, очень неудобный',
-      es: 'Macizo y fornido, prbablamente incómodo.'
+      es: 'Macizo y fornido, prbablamente incómodo.',
+      fr: 'Chunky dans ses regards, probablement très inconfortable'
     }
   },
 
@@ -76,7 +85,8 @@ export default [
     description: {
       en: 'Run as quick as the wind....or close to it',
       ru: 'Беги так быстро, как ветер .... ну или близко к нему',
-      es: 'Con estos zapatos puedes ver el viento y llevarlo contigo.'
+      es: 'Con estos zapatos puedes ver el viento y llevarlo contigo.',
+      fr: 'Courez aussi vite que le vent....ou proche'
     }
   },
 
@@ -85,7 +95,8 @@ export default [
     description: {
       en: 'Forged without heat, runs without stumble',
       ru: 'Кованые без тепла, беги без спотыкания',
-      es: 'Forjado sin calor, te dejan correr sin tropezar.'
+      es: 'Forjado sin calor, te dejan correr sin tropezar.',
+      fr: 'Forgé sans chaleur, fonctionne sans trébucher'
     }
   },
 
@@ -94,7 +105,8 @@ export default [
     description: {
       en: 'Some say you can run on clouds with these. The few that have tried have already ascended.',
       ru: 'Некоторые говорят, что с этими ботинками можно бегать на облаках. Те немногие, которые пытались, уже вознеслись.',
-      es: '¡Corre por las nubes con las nuevas Air Miguels!'
+      es: '¡Corre por las nubes con las nuevas Air Miguels!',
+      fr: 'Certains disent que vous pouvez courir sur des nuages avec ceux-ci. Les rares qui ont essayé sont déjà ascensionné'
     }
   },
 
@@ -103,7 +115,8 @@ export default [
     description: {
       en: 'Ancient boots rumoured to be stolen from the feet of the Roman messenger god....',
       ru: 'По слухам, древние сапоги были украдены у ног римского бога-вестника ...',
-      es: 'Botas del mesajero mentiroso, del espía nocturno, del dios griego de apoyar y asesinar.'
+      es: 'Botas del mesajero mentiroso, del espía nocturno, del dios griego de apoyar y asesinar.',
+      fr: 'Bottes anciennes qu\'ils sont volés aux pieds de dieu messager romain....'
     }
   },
 
@@ -112,7 +125,8 @@ export default [
     description: {
       en: 'Stars were always considered to be all-mighty, now the\'re yours',
       ru: 'Звезды всегда считались всемогущими, теперь они твои',
-      es: 'Las armas mas peligrosas del universo, chanclas imbuido con la ira y furia de una madre cariñosa.'
+      es: 'Las armas mas peligrosas del universo, chanclas imbuido con la ira y furia de una madre cariñosa.',
+      fr: 'Les étoiles ont toujours été considérées comme tout-puissant, maintenant ils sont à vous'
     }
   }
 
